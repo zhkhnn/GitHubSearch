e663155416fa6c9805b5086ded4703abb1d2eea2
zhkhnn <zhakhanaruzhan@gmail.com>
2023-11-05 22:21:11 +0600

    Initial Commit

---
+++ Zimran/ZimranApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZimranApp.swift
+//  Zimran
+//
+//  Created by Aruzhan Zhakhan on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZimranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
