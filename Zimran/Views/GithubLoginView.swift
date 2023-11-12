dc1bce79a6e9b975db653d40260a46b59aad40fe
zhkhnn <zhakhanaruzhan@gmail.com>
2023-11-12 18:55:50 +0600

    wtf piece of shit is not working

--- Zimran/Views/GithubLoginView.swift
+++ Zimran/Views/GithubLoginView.swift
@@ -6,7 +6,7 @@ struct GithubLoginView: View {
 
     var body: some View {
         if isLogged {
-            Text("Logged in!")
+            ContentView()
         } else {
             Button("Login with GitHub") {
                 performGitHubLogin()
@@ -17,28 +17,27 @@ struct GithubLoginView: View {
     }
 
     private func performGitHubLogin() {
-//        guard let url = URL(string: "https://github.com/login/oauth/authorize") else { return }
-
         let oauthswift = OAuth2Swift(
             consumerKey:    "1f5c31cbadf064c8dc39",
-            consumerSecret: "5ff87e573c01cf7226be4fa7b7f2ed009f53b927",
+            consumerSecret: "b5b08d330d88a4889b59f5761a5ce47b1e146357",
             authorizeUrl: "https://github.com/login/oauth/authorize",
             accessTokenUrl: "https://github.com/login/oauth/access_token",
             responseType: "code"
         )
         oauthswift.authorize(
-                    withCallbackURL: URL(string: "Aruzhan-Zhakhan.Zimran://callback")!,
+                    withCallbackURL: URL(string: "Zimran://")!,
                     scope: "user",
                     state: "your_state",
                     completionHandler: { result in
                         switch result {
                         case .success(let (credential, _, _)):
                             print("Access Token: \(credential.oauthToken)")
-                            self.isLogged = true
+                            
                             oauthswift.client.get(
                                 "https://api.github.com/user"){ result in
                                     switch result {
                                     case .success(let response):
+                                        self.isLogged = true
                                         let dataString = response.string
                                         print(dataString)
                                     case .failure(let error):
